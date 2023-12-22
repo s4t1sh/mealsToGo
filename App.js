067c2f044f81e7d739870a9cdf6f6995ebbae792
V Satish <vasettisatish@gmail.com>
2023-12-22 12:00:42 +0530

    meals

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import { SafeAreaView, StyleSheet, Text, View, StatusBar} from 'react-native';
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Page content</Text>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'skyblue',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 25,
+    fontWeight: '500',
+  },
 });
+
+export default App;
